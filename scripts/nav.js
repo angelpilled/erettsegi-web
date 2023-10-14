c6b82f5fe1bd5cf4a6ed0936a231988f2079a51d
angelpilled <angelpilleddev@gmail.com>
2023-10-14 17:55:53 +0200

    Nav hamburger menu mindjart keszen, kisebb javitasok, uj kepek, satobbi

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,3 @@
-console.log("Hello");
 $(document).ready(function() {
         // Transition effect for navbar 
         $(window).scroll(function() {
@@ -10,3 +9,43 @@ $(document).ready(function() {
           }
         });
 });
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-left');
+  const navLinks = document.querySelectorAll('.nav-left li');
+  const close = document.querySelector('.close');
+  const bodyTag = document.getElementsByTagName("BODY")[0]
+  
+  //Toggle Nav
+  burger.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
+    
+    //Animate Links
+    navLinks.forEach((link, index)=>{
+      if(link.style.animation){
+        link.style.animation = ''
+      }else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+
+      }
+    });
+    
+    //burger animation
+    burger.classList.toggle('toggle');
+	bodyTag.classList.toggle("no-scroll")
+    
+    
+  });
+
+	close.addEventListener('click', ()=> {
+		nav.classList.toggle('nav-active');
+		bodyTag.classList.toggle("no-scroll")
+	});
+  
+  
+  
+  
+}
+
+navSlide();
